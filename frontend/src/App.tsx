9eda19fa1e3de68d6ca9f01640844af1e6923891
Dev750wala <sadisatsowaladev1@gmail.com>
2024-08-07 08:28:56 +0530

    first version UD

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { WavyBackground } from "./components/ui/wavy-background";
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="fixed inset-0 w-full h-full z-[-1]">
+        <WavyBackground className="max-w-4xl mx-auto pb-40 overflow-x-hidden" />
+        <div className="relative z-10">
+          {/* Your page content */}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {/* <div className="">
+      </div> */}
     </>
   )
 }
